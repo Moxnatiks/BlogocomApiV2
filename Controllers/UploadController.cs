71f43d470379749adcc7c3dc64cda039b10800b1
Moxnatiks <yaroslav.protsenko@nure.ua>
2021-11-10 14:17:07 +0200

    squeeze

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -17,17 +17,166 @@ namespace BlogocomApiV2.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
+        private const string DirFilePath = "Files";
         private readonly ApiDbContext DB;
         public IConfiguration Configuration { get; }
 
-        
+
         public UploadController (ApiDbContext context, IConfiguration configuration)
         {
             Configuration = configuration;
             DB = context;
         }
 
-        
+        [HttpPost]
+        [Route("upload/picture/squeeze")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Models.File>> UploadSqueezePicture(
+            IFormFile file,
+            CancellationToken cancellationToken)
+        {
+            if (CheckAccessPictureFile(file))
+            {
+                Models.File? pic = await WritePictureSqueezeFile(file);
+                if (pic != null)
+                {
+                    //DB.Files.Add(pic);
+                    //DB.SaveChanges();
+                    return Ok(pic);
+                }
+                else return BadRequest(new { message = "Error while loading!" });
+            }
+            else
+            {
+                return BadRequest(new { message = "Invalid file extension!" });
+            }
+        }
+
+        private bool CheckAccessPictureFile(IFormFile file)
+        {
+            var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+            return (extension == ".tiff" ||
+                    extension == ".jpg" ||
+                    extension == ".raw" ||
+                    extension == ".jpeg" ||
+                    extension == ".bmp" ||
+                    extension == ".gif" ||
+                    extension == ".png"); // Change the extension based on your need
+        }
+        private async Task<Models.File> WritePictureSqueezeFile(IFormFile file)
+        {
+            //string DirFilePath = "Files";
+            string PreviewVideoPicWebPart = "";
+
+            string fileName;
+            Models.File? pic = null;
+            try
+            {
+                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+                var ticks = DateTime.Now.Ticks;
+                fileName = ticks + extension; //Create a new Name for the file due to security reasons.
+
+                createDirectory();
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), DirFilePath, fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                MagickImage image = new MagickImage(DirFilePath +"//"+ fileName); // Получить объект изображения
+                image.Quality = 100; // Выполнить сжатие без потерь
+                image.Resize(1000, 1000); // Общая настройка размера
+                string SaveToPath = Path.Combine(Directory.GetCurrentDirectory(), DirFilePath, ticks + "squeeze" + extension); // Редактировать путь сохранения
+                image.Write(SaveToPath); // Запись в целевой путь в потоке
+                image.Dispose(); // Объект освобожден
+
+                FileInfo fileInfo = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), DirFilePath, ticks + "squeeze" + extension));
+
+                pic = new Models.File
+                {
+                    OriginalName = file.FileName,
+                    UniqueName = ticks + "squeeze" + extension,
+                    Size = fileInfo.Length,
+                    Type = extension.Replace(".", ""),
+                    PreviewVideoPicWebPart = PreviewVideoPicWebPart,
+                    WebPath = Configuration.GetConnectionString("Domen") + "/api/file/download/" + ticks + "squeeze" + extension,
+                };
+
+
+                if (System.IO.File.Exists(Path.Combine(Directory.GetCurrentDirectory(), DirFilePath, fileName)))
+                {
+                    System.IO.File.Delete(Path.Combine(Directory.GetCurrentDirectory(), DirFilePath, fileName));
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return pic;
+        }
+
+        private void createDirectory()
+        {
+            try
+            {
+                string pathFile = Path.Combine(Directory.GetCurrentDirectory(), DirFilePath);
+
+                if (!Directory.Exists(pathFile))
+                {
+                    Directory.CreateDirectory(pathFile);
+                }
+            }
+            catch(Exception e) { }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //[Authorize]
         [HttpPost]
@@ -123,32 +272,6 @@ namespace BlogocomApiV2.Controllers
 
 
 
-               /* if (extension == ".tiff"||
-                    extension == ".jpg" ||
-                    extension == ".raw" ||
-                    extension == ".jpeg"||
-                    extension == ".bmp" ||
-                    extension == ".gif" ||
-                    extension == ".png" )
-                {
-                    var textToWrite = "Insert This Text Into Image";
-
-                    using (var image = new MagickImage(file.OpenReadStream()))
-                    {
-                        using (var caption = new MagickImage($"caption:{textToWrite}", readSettings))
-                        {
-                            // Add the caption layer on top of the background image
-                            // at position 590,450
-                            image.Composite(caption, 590, 450, CompositeOperator.Over);
-
-                            image.Write(pathToNewImage);
-                        }
-                    }
-
-                }*/
-                
-
-
                 pic = new Models.File
                 {
                     OriginalName = file.FileName,
@@ -161,7 +284,7 @@ namespace BlogocomApiV2.Controllers
             }
             catch (Exception e)
             {
-                //log error
+                Console.WriteLine(e.Message);
             }
 
             return pic;
@@ -171,7 +294,7 @@ namespace BlogocomApiV2.Controllers
         [HttpGet("download/{uniqueName}")]
         public async Task<ActionResult> DownloadFile(string uniqueName)
         {
-            string filePath = "Files/" + uniqueName; // Here, you should validate the request and the existance of the file.
+            string filePath = (Path.Combine(Directory.GetCurrentDirectory(), DirFilePath, uniqueName)); // Here, you should validate the request and the existance of the file.
             byte[] bytes ;
             try
             {
@@ -186,3 +309,4 @@ namespace BlogocomApiV2.Controllers
         }
     }
 }
+
